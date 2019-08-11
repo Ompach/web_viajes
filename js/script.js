40d1ca6e99c9494d296f5233ec8b10d902ad6dd5
ompach <omarheycom@gmail.com>
2019-08-10 22:42:44 -0600

    El efecto de parallax esta terminado hasta este punto funciona muy bien

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@
 /* -------------------------------------------------------------------------- */
 
 $(".fade-slider").jdSlider({
-    slideShow: 3,
+    slideShow: 5,
     isAuto: true,
     isLoop: true,
     interval: 6000,
     isCursor: true,
 
+
+   
+
 });
+
+
+var alturaBanner = $(".fade-slider").height();
+$(".bannerParallax").css({"height": alturaBanner+"px"})
+ 
