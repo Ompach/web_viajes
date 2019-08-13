f9c7d6da380b6dfdbf3a7bd266169afd8be05e11
ompach <omarheycom@gmail.com>
2019-08-13 17:44:15 -0600

    categorias superirores agregados los espacios para categorias

--- js/script.js
+++ js/script.js
@@ -2,18 +2,6 @@
 /*                                   banner                                   */
 /* -------------------------------------------------------------------------- */
 
-$(".fade-slider").jdSlider({
-    isSliding: false,
-    isAuto: true,
-    isLoop: true,
-    interval: 5000,
-    isCursor: false,
-    resize : true,
-    flexible : true,
-    isDrag: false,
-    speed: 3000,
-});
-
 
 
 
